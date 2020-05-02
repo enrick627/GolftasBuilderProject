2c29d4e91fd539c4ccbf78621778645ed66c6e3b
enrick627 <enrick.demunter@student.romerocollege.be>
2020-05-02 12:47:50 +0200

    Add project files.

---
+++ GolftasBuilderProject/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace GolftasBuilderProject
+{
+    partial class Form_GolftasBuilder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_driver = new System.Windows.Forms.TextBox();
+            this.label_Driver = new System.Windows.Forms.Label();
+            this.LAbel_3Wood = new System.Windows.Forms.Label();
+            this.label_5wood = new System.Windows.Forms.Label();
+            this.label_ijzers = new System.Windows.Forms.Label();
+            this.label_Wedges = new System.Windows.Forms.Label();
+            this.label_Putter = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_3wood = new System.Windows.Forms.TextBox();
+            this.textBox_5wood = new System.Windows.Forms.TextBox();
+            this.textBox_ijzers = new System.Windows.Forms.TextBox();
+            this.textBox1_wedges = new System.Windows.Forms.TextBox();
+            this.textBox_putters = new System.Windows.Forms.TextBox();
+            this.button_close = new System.Windows.Forms.Button();
+            this.Button_ToonGolftas = new System.Windows.Forms.Button();
+            this.Textbox_TotaleGolftas = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_driver
+            // 
+            this.textBox_driver.Location = new System.Drawing.Point(226, 40);
+            this.textBox_driver.Name = "textBox_driver";
+            this.textBox_driver.Size = new System.Drawing.Size(100, 22);
+            this.textBox_driver.TabIndex = 0;
+            // 
+            // label_Driver
+            // 
+            this.label_Driver.AutoSize = true;
+            this.label_Driver.Location = new System.Drawing.Point(58, 44);
+            this.label_Driver.Name = "label_Driver";
+            this.label_Driver.Size = new System.Drawing.Size(50, 17);
+            this.label_Driver.TabIndex = 1;
+            this.label_Driver.Text = "Driver:";
+            // 
+            // LAbel_3Wood
+            // 
+            this.LAbel_3Wood.AutoSize = true;
+            this.LAbel_3Wood.Location = new System.Drawing.Point(58, 84);
+            this.LAbel_3Wood.Name = "LAbel_3Wood";
+            this.LAbel_3Wood.Size = new System.Drawing.Size(57, 17);
+            this.LAbel_3Wood.TabIndex = 2;
+            this.LAbel_3Wood.Text = "3Wood:";
+            // 
+            // label_5wood
+            // 
+            this.label_5wood.AutoSize = true;
+            this.label_5wood.Location = new System.Drawing.Point(58, 118);
+            this.label_5wood.Name = "label_5wood";
+            this.label_5wood.Size = new System.Drawing.Size(53, 17);
+            this.label_5wood.TabIndex = 3;
+            this.label_5wood.Text = "5 wood";
+            // 
+            // label_ijzers
+            // 
+            this.label_ijzers.AutoSize = true;
+            this.label_ijzers.Location = new System.Drawing.Point(58, 152);
+            this.label_ijzers.Name = "label_ijzers";
+            this.label_ijzers.Size = new System.Drawing.Size(154, 17);
+            this.label_ijzers.TabIndex = 4;
+            this.label_ijzers.Text = "ijzer set model en merk";
+            // 
+            // label_Wedges
+            // 
+            this.label_Wedges.AutoSize = true;
+            this.label_Wedges.Location = new System.Drawing.Point(58, 190);
+            this.label_Wedges.Name = "label_Wedges";
+            this.label_Wedges.Size = new System.Drawing.Size(60, 17);
+            this.label_Wedges.TabIndex = 5;
+            this.label_Wedges.Text = "Wedges";
+            // 
+            // label_Putter
+            // 
+            this.label_Putter.AutoSize = true;
+            this.label_Putter.Location = new System.Drawing.Point(61, 224);
+            this.label_Putter.Name = "label_Putter";
+            this.label_Putter.Size = new System.Drawing.Size(46, 17);
+            this.label_Putter.TabIndex = 6;
+            this.label_Putter.Text = "Putter";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(958, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Dit is mijn bag builder. De bedoeling is dat u het merk en het model van de club " +
+    "of clubs invult en zo krijgt u een goed overzicht over uw nieuwe golftas.";
+            // 
+            // textBox_3wood
+            // 
+            this.textBox_3wood.Location = new System.Drawing.Point(226, 78);
+            this.textBox_3wood.Name = "textBox_3wood";
+            this.textBox_3wood.Size = new System.Drawing.Size(100, 22);
+            this.textBox_3wood.TabIndex = 8;
+            // 
+            // textBox_5wood
+            // 
+            this.textBox_5wood.Location = new System.Drawing.Point(226, 118);
+            this.textBox_5wood.Name = "textBox_5wood";
+            this.textBox_5wood.Size = new System.Drawing.Size(100, 22);
+            this.textBox_5wood.TabIndex = 9;
+            // 
+            // textBox_ijzers
+            // 
+            this.textBox_ijzers.Location = new System.Drawing.Point(226, 152);
+            this.textBox_ijzers.Name = "textBox_ijzers";
+            this.textBox_ijzers.Size = new System.Drawing.Size(100, 22);
+            this.textBox_ijzers.TabIndex = 10;
+            // 
+            // textBox1_wedges
+            // 
+            this.textBox1_wedges.Location = new System.Drawing.Point(226, 187);
+            this.textBox1_wedges.Name = "textBox1_wedges";
+            this.textBox1_wedges.Size = new System.Drawing.Size(100, 22);
+            this.textBox1_wedges.TabIndex = 11;
+            // 
+            // textBox_putters
+            // 
+            this.textBox_putters.Location = new System.Drawing.Point(226, 218);
+            this.textBox_putters.Name = "textBox_putters";
+            this.textBox_putters.Size = new System.Drawing.Size(100, 22);
+            this.textBox_putters.TabIndex = 12;
+            // 
+            // button_close
+            // 
+            this.button_close.Location = new System.Drawing.Point(949, 377);
+            this.button_close.Name = "button_close";
+            this.button_close.Size = new System.Drawing.Size(85, 43);
+            this.button_close.TabIndex = 13;
+            this.button_close.Text = "close";
+            this.button_close.UseVisualStyleBackColor = true;
+            this.button_close.Click += new System.EventHandler(this.button_close_Click);
+            // 
+            // Button_ToonGolftas
+            // 
+            this.Button_ToonGolftas.Location = new System.Drawing.Point(61, 275);
+            this.Button_ToonGolftas.Name = "Button_ToonGolftas";
+            this.Button_ToonGolftas.Size = new System.Drawing.Size(122, 58);
+            this.Button_ToonGolftas.TabIndex = 14;
+            this.Button_ToonGolftas.Text = "toon uw nieuwe golftas";
+            this.Button_ToonGolftas.UseVisualStyleBackColor = true;
+            this.Button_ToonGolftas.Click += new System.EventHandler(this.Button_ToonGolftas_Click);
+            // 
+            // Textbox_TotaleGolftas
+            // 
+            this.Textbox_TotaleGolftas.Location = new System.Drawing.Point(420, 238);
+            this.Textbox_TotaleGolftas.Multiline = true;
+            this.Textbox_TotaleGolftas.Name = "Textbox_TotaleGolftas";
+            this.Textbox_TotaleGolftas.Size = new System.Drawing.Size(224, 182);
+            this.Textbox_TotaleGolftas.TabIndex = 15;
+            this.Textbox_TotaleGolftas.TextChanged += new System.EventHandler(this.Textbox_TotaleGolftas_TextChanged);
+            // 
+            // Form_GolftasBuilder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1046, 432);
+            this.Controls.Add(this.Textbox_TotaleGolftas);
+            this.Controls.Add(this.Button_ToonGolftas);
+            this.Controls.Add(this.button_close);
+            this.Controls.Add(this.textBox_putters);
+            this.Controls.Add(this.textBox1_wedges);
+            this.Controls.Add(this.textBox_ijzers);
+            this.Controls.Add(this.textBox_5wood);
+            this.Controls.Add(this.textBox_3wood);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_Putter);
+            this.Controls.Add(this.label_Wedges);
+            this.Controls.Add(this.label_ijzers);
+            this.Controls.Add(this.label_5wood);
+            this.Controls.Add(this.LAbel_3Wood);
+            this.Controls.Add(this.label_Driver);
+            this.Controls.Add(this.textBox_driver);
+            this.Name = "Form_GolftasBuilder";
+            this.Text = "Golftas Builder";
+            this.Load += new System.EventHandler(this.Form_GolftasBuilder_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_driver;
+        private System.Windows.Forms.Label label_Driver;
+        private System.Windows.Forms.Label LAbel_3Wood;
+        private System.Windows.Forms.Label label_5wood;
+        private System.Windows.Forms.Label label_ijzers;
+        private System.Windows.Forms.Label label_Wedges;
+        private System.Windows.Forms.Label label_Putter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_3wood;
+        private System.Windows.Forms.TextBox textBox_5wood;
+        private System.Windows.Forms.TextBox textBox_ijzers;
+        private System.Windows.Forms.TextBox textBox1_wedges;
+        private System.Windows.Forms.TextBox textBox_putters;
+        private System.Windows.Forms.Button button_close;
+        private System.Windows.Forms.Button Button_ToonGolftas;
+        private System.Windows.Forms.TextBox Textbox_TotaleGolftas;
+    }
+}
+
---
+++ GolftasBuilderProject/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GolftasBuilderProject
+{
+    public partial class Form_GolftasBuilder : Form
+    {
+        public Form_GolftasBuilder()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void Form_GolftasBuilder_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_close_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_ToonGolftas_Click(object sender, EventArgs e)
+        {
+            string Driver, DrieWood, VijfWood, Ijzers, Wedges, Putters;
+            Driver = textBox_driver.ToString();
+            DrieWood = textBox_3wood.ToString();
+            VijfWood = textBox_5wood.ToString();
+            Ijzers = textBox_ijzers.ToString();
+            Wedges = textBox1_wedges.ToString();
+            Putters = textBox_putters.ToString();
+            this.Textbox_TotaleGolftas.Text = ($"driver:{Driver} 3 wood:{DrieWood} 5 wood: {VijfWood} ijzers: {Ijzers} wedges: {Wedges} putters: {Putters}");
+            
+        }
+
+        private void Textbox_TotaleGolftas_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
